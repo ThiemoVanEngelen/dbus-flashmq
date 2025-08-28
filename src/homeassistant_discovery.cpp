7e918410fc6c28c47db19eb3f1c12c4c36ee8d15
Thiemo van Engelen <tvanengelen@victronenergy.com>
2025-08-28 12:10:01 +0200

    Add AC charger support to Home Assistant discovery

--- src/homeassistant_discovery.cpp
+++ src/homeassistant_discovery.cpp
@@ -88,7 +88,8 @@ std::unordered_map<std::string_view, std::function<std::unique_ptr<HomeAssistant
     {"tank", []() { return std::make_unique<HomeAssistantDiscovery::TankDevice>(); }},
     {"grid", []() { return std::make_unique<HomeAssistantDiscovery::GridMeterDevice>(); }},
     {"switch", []() { return std::make_unique<HomeAssistantDiscovery::SwitchDevice>(); }},
-    {"meteo", []() { return std::make_unique<HomeAssistantDiscovery::MeteoDevice>(); }}
+    {"meteo", []() { return std::make_unique<HomeAssistantDiscovery::MeteoDevice>(); }},
+    {"charger", []() { return std::make_unique<HomeAssistantDiscovery::ChargerDevice>(); }},
 };
 
 void HomeAssistantDiscovery::DeviceData::addNumericDiagnostic(std::string_view dbus_path,
@@ -639,6 +640,116 @@ std::pair<std::string, std::string> HomeAssistantDiscovery::SolarChargerDevice::
     return {name, model};
 }
 
+void HomeAssistantDiscovery::ChargerDevice::addEntities(const std::unordered_map<std::string, std::unordered_map<std::string, Item>>& all_items)
+{
+    const auto& service_items = all_items.at(service);
+
+    if (service_items.contains("/Dc/0/Voltage")) {
+        HAEntityConfig bat_voltage;
+        bat_voltage.name = "Battery Voltage";
+        bat_voltage.device_class = "voltage";
+        bat_voltage.state_class = "measurement";
+        bat_voltage.unit_of_measurement = "V";
+        bat_voltage.icon = "mdi:battery-charging";
+        bat_voltage.suggested_display_precision = 2;
+        entities.emplace("/Dc/0/Voltage", std::move(bat_voltage));
+    }
+    if (service_items.contains("/Dc/0/Current")) {
+        HAEntityConfig bat_current;
+        bat_current.name = "Battery Current";
+        bat_current.device_class = "current";
+        bat_current.state_class = "measurement";
+        bat_current.unit_of_measurement = "A";
+        bat_current.icon = "mdi:battery-charging";
+        bat_current.suggested_display_precision = 2;
+        entities.emplace("/Dc/0/Current", std::move(bat_current));
+    }
+    if (service_items.contains("/Dc/1/Voltage")) {
+        HAEntityConfig bat_voltage;
+        bat_voltage.name = "Battery Voltage (2)";
+        bat_voltage.device_class = "voltage";
+        bat_voltage.state_class = "measurement";
+        bat_voltage.unit_of_measurement = "V";
+        bat_voltage.icon = "mdi:battery-charging";
+        bat_voltage.suggested_display_precision = 2;
+        entities.emplace("/Dc/1/Voltage", std::move(bat_voltage));
+    }
+    if (service_items.contains("/Dc/1/Current")) {
+        HAEntityConfig bat_current;
+        bat_current.name = "Battery Current (2)";
+        bat_current.device_class = "current";
+        bat_current.state_class = "measurement";
+        bat_current.unit_of_measurement = "A";
+        bat_current.icon = "mdi:battery-charging";
+        bat_current.suggested_display_precision = 2;
+        entities.emplace("/Dc/1/Current", std::move(bat_current));
+    }
+    if (service_items.contains("/Dc/2/Voltage")) {
+        HAEntityConfig bat_voltage;
+        bat_voltage.name = "Battery Voltage (3)";
+        bat_voltage.device_class = "voltage";
+        bat_voltage.state_class = "measurement";
+        bat_voltage.unit_of_measurement = "V";
+        bat_voltage.icon = "mdi:battery-charging";
+        bat_voltage.suggested_display_precision = 2;
+        entities.emplace("/Dc/2/Voltage", std::move(bat_voltage));
+    }
+    if (service_items.contains("/Dc/2/Current")) {
+        HAEntityConfig bat_current;
+        bat_current.name = "Battery Current (3)";
+        bat_current.device_class = "current";
+        bat_current.state_class = "measurement";
+        bat_current.unit_of_measurement = "A";
+        bat_current.icon = "mdi:battery-charging";
+        bat_current.suggested_display_precision = 2;
+        entities.emplace("/Dc/2/Current", std::move(bat_current));
+    }
+    if (service_items.contains("/Ac/In/L1/P")) {
+        HAEntityConfig power_sensor;
+        power_sensor.name = "AC Power";
+        power_sensor.device_class = "power";
+        power_sensor.state_class = "measurement";
+        power_sensor.unit_of_measurement = "W";
+        power_sensor.icon = "mdi:transmission-tower";
+        power_sensor.suggested_display_precision = 1;
+        entities.emplace("/Ac/In/L1/P", std::move(power_sensor));
+    }
+    if (service_items.contains("/Ac/In/L1/I")) {
+        HAEntityConfig current_sensor;
+        current_sensor.name = "AC Current";
+        current_sensor.device_class = "current";
+        current_sensor.state_class = "measurement";
+        current_sensor.unit_of_measurement = "A";
+        current_sensor.icon = "mdi:current-ac";
+        current_sensor.suggested_display_precision = 2;
+        entities.emplace("/Ac/In/L1/I", std::move(current_sensor));
+    }
+    if (service_items.contains("/State")) {
+        HAEntityConfig state_sensor;
+        state_sensor.name = "Charge State";
+        state_sensor.icon = "mdi:battery-charging";
+        // Custom value template for state enum
+        state_sensor.value_template = CHARGER_STATE_VALUE_TEMPLATE;
+        entities.emplace("/State", std::move(state_sensor));
+    }
+    addCommonDiagnostics(service_items);
+}
+std::pair<std::string, std::string> HomeAssistantDiscovery::ChargerDevice::getNameAndModel(const std::unordered_map<std::string, std::unordered_map<std::string, Item>>& all_items)
+{
+    const auto& items = all_items.at(service);
+    std::string name = getItemText(items, {"/CustomName"});
+    if (name.empty()) {
+        name = "AC Charger " + std::string(short_service_name.instance());
+    }
+
+    std::string model = getItemText(items, {"/ProductName"});
+    if (model.empty()) {
+        model = "AC Charger";
+    }
+
+    return {name, model};
+}
+
 void HomeAssistantDiscovery::VeBusDevice::addEntities(const std::unordered_map<std::string, std::unordered_map<std::string, Item>>& all_items)
 {
     const auto& service_items = all_items.at(service);
@@ -1045,7 +1156,6 @@ void HomeAssistantDiscovery::SwitchDevice::addEntities(const std::unordered_map<
         addStringDiagnostic("/State", "Device State", "mdi:power-settings", "{% set states = {256: 'Connected', 257: 'Over temperature', 258: 'Temperature warning', 259: 'Channel fault', 260: 'Channel Tripped', 261: 'Under Voltage'} %}{{ states[value_json.value] | default('Unknown (' + value_json.value|string + ')') }}");
     addCommonDiagnostics(service_items);
 }
-
 std::pair<std::string, std::string> HomeAssistantDiscovery::SwitchDevice::getNameAndModel(const std::unordered_map<std::string, std::unordered_map<std::string, Item>>& all_items)
 {
     const auto& items = all_items.at(service);
--- src/homeassistant_discovery.h
+++ src/homeassistant_discovery.h
@@ -156,7 +156,10 @@ private:
         std::pair<std::string, std::string> getNameAndModel(const std::unordered_map<std::string, std::unordered_map<std::string, Item>> &all_items) override;
         void addEntities(const std::unordered_map<std::string, std::unordered_map<std::string, Item>> &all_items) override;
     };
-
+    struct ChargerDevice : DeviceData {
+        std::pair<std::string, std::string> getNameAndModel(const std::unordered_map<std::string, std::unordered_map<std::string, Item>> &all_items) override;
+        void addEntities(const std::unordered_map<std::string, std::unordered_map<std::string, Item>> &all_items) override;
+    };
 
     std::string discovery_prefix = "homeassistant";
     std::string vrm_id;
